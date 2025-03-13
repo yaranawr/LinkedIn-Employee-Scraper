e6a6dba67c8af4e3a329c253f389216a93a23a20
Yarana <yaranawr@gmail.com>
2025-03-13 17:28:37 -0300

    Fixed LinkedIn script to adapt to frequent code updates

--- linkedin_employee_scraper.py
+++ linkedin_employee_scraper.py
@@ -1,15 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 import os
 import sys
 import time
 
-if len(sys.argv) < 3:
-    print('Usage: python linkedin_employee_scraper.py "https://www.linkedin.com/COMPANY/people/" "path/to/chromedriver"')
+if len(sys.argv) < 2:
+    print('Usage: python linkedin_employee_scraper.py "https://www.linkedin.com/COMPANY/people/"')
     sys.exit(1)
-
-service = Service(sys.argv[2])
 
 home_dir = os.path.expanduser("~")
 
@@ -28,6 +27,7 @@ option.add_argument("--log-level=3")
 option.add_argument(f"--user-data-dir={user_data_dir}")
 option.add_argument(f"--profile-directory={profile_dir}")
 
+service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=option)
 driver.get(sys.argv[1])
 
@@ -52,11 +52,11 @@ error_count = 0
 with open('linkedin_profiles.csv', 'w', encoding='utf-8') as file:
     for profile in profiles:
         try:
-            link_element = profile.find_element(By.CSS_SELECTOR, "a.app-aware-link")
+            link_element = profile.find_element(By.CSS_SELECTOR, "a[data-test-app-aware-link]") 
             
             profile_link = link_element.get_attribute("href").split('?')[0]
-            name = profile.find_element(By.CLASS_NAME, 'org-people-profile-card__profile-title').text
-            description = profile.find_element(By.CLASS_NAME, 'artdeco-entity-lockup__subtitle').text
+            name = profile.find_element(By.CSS_SELECTOR, 'div.lt-line-clamp.lt-line-clamp--single-line').text 
+            description = profile.find_element(By.CSS_SELECTOR, 'div.lt-line-clamp.lt-line-clamp--multi-line').text
             
             file.write(f"{profile_link};{name};{description}\n")
         
@@ -64,7 +64,8 @@ with open('linkedin_profiles.csv', 'w', encoding='utf-8') as file:
             error_count += 1
 
 if error_count > 0:
-    print(f"Total profiles with errors: {error_count}")
+    print(f"Total profiles with errors: {error_count}.")
+    print("If other profiles were processed without issues, the ones with errors are most likely private.")
 else:
     print("All profiles processed successfully.")
 
